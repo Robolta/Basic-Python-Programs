b973e16cd19384e8526f396ea34e34c8f1cb3efc
Robolta <ameliakgreenwood@gmail.com>
2025-08-05 23:17:07 -0400

    Updated fizzbuzz.py to fit guidelines

--- Popular/fizzbuzz.py
+++ Popular/fizzbuzz.py
@@ -1,14 +1,34 @@
-for i in range(1, 101):
+def fizzbuzz(upper_limit):
+	"""
+	Yields strings based on the rules of "Fizzbuzz".
+	Iterates starting at 1 up to the upper_limit.
+	If a number is divisible by 3, yield "Fizz".
+	If a number is divisible by 5, yield "Buzz".
+	Otherwise, simply yield the number.
 
-	output = ""
-	
-	if i % 3 == 0:
-		output += "Fizz"
+	Args:
+		upper_limit (int): The highest number used when iterating.
 
-	if i % 5 == 0:
-		output += "Buzz"
+	Yields:
+		str: The string for the current number.
+	"""
+	for value in range(1, upper_limit + 1):
 
-	if output == "":
-		output = str(i)
+		output = ""
+		
+		if value % 3 == 0:
+			output += "Fizz"
 
-	print(output)
+		if value % 5 == 0:
+			output += "Buzz"
+
+		if output == "":
+			output = str(value)
+
+		yield output
+
+
+
+if __name__ == "__main__":
+	for value in fizzbuzz(100):
+		print(value)
