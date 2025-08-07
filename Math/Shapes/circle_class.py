6c040df76b290ecc9ce4580127e2577e3645d20c
Robolta <ameliakgreenwood@gmail.com>
2025-08-06 23:30:13 -0400

    Updated typing based on style guide

--- Math/Shapes/circle_class.py
+++ Math/Shapes/circle_class.py
@@ -1,14 +1,14 @@
 from math import PI
 
 class Circle:
-	def __init__(self, radius):
+	def __init__(self, radius: int | float):
 		self.radius = radius
 	
-	def get_diameter(self):
+	def get_diameter(self) -> int | float:
 		return 2 * self.radius
 	
-	def get_circumference(self):
+	def get_circumference(self) -> float:
 		return 2 * PI * self.radius
 	
-	def get_area(self):
+	def get_area(self) -> float:
 		return PI * self.radius * self.radius
